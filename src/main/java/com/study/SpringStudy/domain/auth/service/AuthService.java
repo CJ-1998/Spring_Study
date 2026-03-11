ca59ef11a67cf496d8d702d944ba316be892052c
CJ <choijun0627@naver.com>
2026-03-11 16:01:20 +0900

    [SPRING-36]  Feat : 로그아웃 로직 구현
    로그아웃 로직 AuthService에 구현
    logout 요청 오면 redis에 jwt - logout 이렇게 key - value 넣음
    jwt 남은 기간동안 key-value 유지

--- src/main/java/com/study/SpringStudy/domain/auth/service/AuthService.java
+++ src/main/java/com/study/SpringStudy/domain/auth/service/AuthService.java
@@ -6,8 +6,11 @@ import com.study.SpringStudy.common.util.JwtProvider;
 import com.study.SpringStudy.domain.user.entity.User;
 import com.study.SpringStudy.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +18,7 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtProvider jwtProvider;
+    private final StringRedisTemplate redisTemplate;
 
     // 로그인 로직
     public String login(String email, String rawPassword) {
@@ -27,5 +31,25 @@ public class AuthService {
 
         // 인증 성공 시 토큰 발급
         return jwtProvider.createToken(user.getId(), user.getRole().name());
+    }
+
+    public void logout(String token) {
+        // 1. 토큰이 유효한지 1차 확인
+        if (!jwtProvider.validateToken(token)) {
+            throw new IllegalArgumentException("유효하지 않은 토큰입니다.");
+        }
+
+        // 2. 토큰의 남은 만료 시간 계산
+        long remainingTime = jwtProvider.getRemainingExpirationTime(token);
+
+        // 3. 남은 시간동안만 Redis에 Blacklist로 저장 (Key: 토큰, Value: "logout")
+        if (remainingTime > 0) {
+            redisTemplate.opsForValue().set(
+                    token,
+                    "logout",
+                    remainingTime,
+                    TimeUnit.MILLISECONDS
+            );
+        }
     }
 }
