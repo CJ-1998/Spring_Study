4de37bcdd8af13ae0872cc130a8f84fa829a7278
CJ <choijun0627@naver.com>
2026-03-11 16:53:25 +0900

    [SPRING-37]  Feat : LoginResponse 생성
    access token, refresh token 반환하는 LoginResponse dto 생성

---
+++ src/main/java/com/study/SpringStudy/domain/auth/dto/response/LoginResponse.java
@@ -0,0 +1,11 @@
+package com.study.SpringStudy.domain.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponse {
+    private String accessToken;
+    private String refreshToken;
+}
